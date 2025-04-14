75a1bae7bad636bf6eb98ac4f3dada4a288b594e
Anuvind Ashok <anuvind.ashok10@gmail.com>
2025-04-14 17:55:21 +0530

    basic UI

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
+import "./styles.css";
 import * as weather from "./weather";
+import logoImage from "./images/logo.png";
 
 function pageLoad() {
+  renderImages();
   const searchBtn = document.querySelector("#searchBtn");
 
   searchBtn.addEventListener("click", (event) => {
@@ -14,6 +17,11 @@ export async function getWeather(location) {
   const weatherData = await weather.fetchWeather(location);
   const weatherInfo = weather.getRequiredInfo(weatherData);
   console.log(weatherInfo);
+}
+
+function renderImages() {
+  const logo = document.querySelector("#app-logo");
+  logo.src = logoImage;
 }
 
 document.addEventListener("DOMContentLoaded", pageLoad);
